4698ae9e1af750f29051805a10ce59f830ef5283
Guillaume Corbelin <guillaume.corbelin@coexya.eu>
2023-02-04 15:29:02 +0100

    feat(modal): animate show hide modal

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,9 +1,22 @@
 function displayModal() {
+  const { body } = document;
   const modal = document.getElementById("contact_modal");
   modal.style.display = "block";
+  body.classList.add("modal-opened");
+  modal.classList.replace("hide", "show");
 }
 
 function closeModal() {
+  const { body } = document;
   const modal = document.getElementById("contact_modal");
-  modal.style.display = "none";
+  const modalContent = document.querySelector(".modal__wrapper");
+  modalContent.classList.add("closing");
+  window.setTimeout(() => {
+    modal.classList.replace("show", "hide");
+  }, 400);
+  window.setTimeout(() => {
+    modal.style.display = "none";
+    modalContent.classList.remove("closing");
+    body.classList.remove("modal-opened");
+  }, 800);
 }
